322845c8a025326dac1709fbcf2cc01287402263
Rakib0061 <rakibulislam0061@gmail.com>
2024-01-10 03:00:23 +0600

    create a tamplate for react js

---
+++ react_template/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-6xl text-yellow-400 h-screen flex items-center justify-center">Hello World!</h1>
+    </>
+  )
+}
+
+export default App
