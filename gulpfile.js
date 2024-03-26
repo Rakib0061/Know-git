b504a7b0fa15df53c58247c43bb1d2e3f765849c
Rakib0061 <rakibulislam0061@gmail.com>
2024-03-27 00:16:33 +0600

    it will also serve multipage template

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+//  🔴gulp plugin :
+import gulp from 'gulp';
+import * as del from 'del';
+import webP from 'gulp-webp';
+import fileInclude from 'gulp-file-include';
+import imageMin, { mozjpeg, optipng } from 'gulp-imagemin';
+
+const { src, dest, series } = gulp;
+
+//  🔴task function :
+//  ########## image task ##########
+function compreseImage() {
+  return src('src/assets/images/**/*.{jpg,png}')
+    .pipe(
+      imageMin([
+        mozjpeg({ quality: 75, progressive: true }),
+        optipng({ optimizationLevel: 2 }),
+      ]),
+    )
+    .pipe(webP())
+    .pipe(dest('src/assets/images'))
+    .on('end', () => {
+      del.deleteSync([
+        'src/assets/images/*.{jpg,png}',
+        '!src/assets/images/*.webp',
+      ]);
+    });
+}
+
+//  ########## html partials task ##########
+function includeHTML() {
+  return src(['*.html', 'src/pages/*.html'])
+    .pipe(fileInclude({ prefix: '@@', basepath: '@file' }))
+    .pipe(
+      dest((file) => {
+        if (file.dirname.includes('src/pages/*.html')) {
+          return './';
+        }
+        return file.dirname;
+      }),
+    );
+}
+
+//  🔴observer
+// if any watch file like sass have to integreate ...
+
+//  🔴export
+export default series(compreseImage, includeHTML);
